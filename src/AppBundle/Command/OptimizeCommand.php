1769a53a0b3b112a996a50917199a43ca1077fce
Aleksandar Dimitrov <netbull2007@gmail.com>
2019-03-26 22:22:31 +0200

    test

--- src/AppBundle/Command/OptimizeCommand.php
+++ src/AppBundle/Command/OptimizeCommand.php
@@ -55,7 +55,7 @@ class OptimizeCommand extends ContainerAwareCommand
         foreach ($finder as $file) {
             copy($file->getRealPath(), $backupDir.str_replace($path, '', $file->getRealPath()));
             var_dump($file->getRealPath());
-            var_dump($backupDir.$file->getFilename());
+            var_dump($backupDir.str_replace($path, '', $file->getRealPath()));
             exit;
 //            $originalFile = $file->getRealPath();
 //            $optimisedFile = $file->getPath().'/'.$file->getBasename('.' . $file->getExtension()).'_optimized.'.$file->getExtension();
