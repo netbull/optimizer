a1f27f286e319ad777157b73a4e10b0b86017d00
Aleksandar Dimitrov <netbull2007@gmail.com>
2019-03-26 22:03:46 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+optimizer
+=========
+
+A Symfony project created on March 26, 2019, 9:57 pm.
---
+++ src/AppBundle/Command/OptimizeCommand.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace AppBundle\Command;
+
+use Spatie\ImageOptimizer\OptimizerChainFactory;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
+
+/**
+ * Class OptimizeCommand
+ * @package App\Command
+ */
+class OptimizeCommand extends ContainerAwareCommand
+{
+    /**
+     * @inheritdoc
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('optimize')
+            ->setDescription('Optimize images')
+            ->addArgument('path', InputArgument::OPTIONAL, 'Path to the image directory.')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $io->title('Optimizing images');
+
+        $path = $input->getArgument('path');
+        if (!$path) {
+            $path = $this->getContainer()->getParameter('kernel.project_dir').'/tmp/';
+        }
+
+        $finder = new Finder();
+        $finder
+            ->in($path)
+            ->files()
+            ->name('*.{jpg,jpeg,png,gif,JPG,JPEG,PNG,GIF}')
+        ;
+
+        $optimizerChain = OptimizerChainFactory::create();
+        /** @var SplFileInfo $file */
+//        foreach ($finder as $file) {
+//            $originalFile = $file->getRealPath();
+//            $optimisedFile = $file->getPath().'/'.$file->getBasename('.' . $file->getExtension()).'_optimized.'.$file->getExtension();
+
+//            $optimizerChain->optimize($originalFile, $optimisedFile);
+//        }
+
+        var_dump(iterator_count($finder));
+
+        $io->success('done');
+    }
+}
