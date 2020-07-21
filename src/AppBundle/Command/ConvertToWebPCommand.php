7f61d1a0a66d3f5fac411be0404d3434487356e1
Aleksandar Dimitrov <netbull2007@gmail.com>
2020-07-21 17:11:46 +0300

    bump versions

--- src/AppBundle/Command/ConvertToWebPCommand.php
+++ src/AppBundle/Command/ConvertToWebPCommand.php
@@ -76,7 +76,7 @@ class ConvertToWebPCommand extends ContainerAwareCommand
 
             $options = [
                 'jpeg' => [
-                    'encoding' => 'lossless',     /* If you are worried about the longer conversion time, you could set it to "lossy" instead (lossy will often be smaller than lossless for jpegs) */
+//                    'encoding' => 'lossless',     /* If you are worried about the longer conversion time, you could set it to "lossy" instead (lossy will often be smaller than lossless for jpegs) */
                     'converters' => ['cwebp'],
 //                    'quality' => 100,      /* Set to same as jpeg (requires imagick or gmagick extension, not necessarily compiled with webp) */
 //                    'max-quality' => 95,      /* Only relevant if quality is set to "auto" */
